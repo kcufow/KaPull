166fa55b6392e39c34321dba7453e4ea2ccfce05
David <kcufow@gmail.com>
2017-11-14 19:43:19 +0800

    first commit

---
+++ app/src/main/java/com/ldw/kapull/ui/activity/RoomActivity.java
@@ -0,0 +1,40 @@
+package com.ldw.kapull.ui.activity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.ldw.kapull.R;
+
+/**
+ * Created by ldw on 2017/11/13.
+ */
+
+public  class RoomActivity extends PureActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public int getRootViewId() {
+        return R.layout.activity_room;
+    }
+
+    @Override
+    public void initUI() {
+
+    }
+}
---
+++ app/src/main/java/com/ldw/kapull/ui/fragment/RoomFragment.java
@@ -0,0 +1,24 @@
+package com.ldw.kapull.ui.fragment;
+
+import com.ldw.xbaselibrary.baseui.XBaseFragment;
+
+/**
+ * Created by ldw on 2017/11/14.
+ */
+
+public class RoomFragment extends XBaseFragment {
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+}
